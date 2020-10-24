8eecc2878237993a2ddff412b36ac41e217a277e
gj-ca <glen.johnson@coderacademy.edu.au>
2020-10-24 12:31:38 +1000

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,28 @@ class UsersController < ApplicationController
     def index
         @users = User.all
     end
+
+    def show
+        # render json: params
+        @user = User.find(params[:user_id])
+    end
     def new
         @user = User.new
+    end
+
+    def edit
+        @user = User.find(params[:user_id])
+    end
+
+    def update
+        # render json: params
+        user = User.find(params[:user_id])
+        user.update!(
+            username: params[:user][:username],
+            password: params[:user][:password]
+        )
+        redirect_to users_path
+
     end
 
     def create
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get "users/new", to: "users#new"
-  post "users", to: "users#create"
   get "users", to: "users#index"
+  get "users/new", to: "users#new"
+  get "users/:user_id", to: "users#show", as: "user"
+  post "users", to: "users#create"
+  get "/users/:user_id/edit", to: "users#edit", as: "edit_user"
+  patch "users/:user_id", to: "users#update"
 end
